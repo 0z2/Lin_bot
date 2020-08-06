11ee9ecb2134cdbe27c17cd3ae44605c9dd0995f
ozz linux <mozzgishere@gmail.com>
2020-08-06 22:08:59 +0500

    added four new achievments

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,13 +1,13 @@
-from .help import dp
+from handlers.users.help import dp
 from .start import dp
-from .work import dp
-from .health import dp
-from .language import dp
-from .sport import dp
-from .studying import dp
-from .delicious_house import dp
-from .music import dp
-from .personal_care import dp
-from .proper_nutrition import dp
+from handlers.users.achievments.work import dp
+from handlers.users.achievments.health import dp
+from handlers.users.achievments.language import dp
+from handlers.users.achievments.sport import dp
+from handlers.users.achievments.studying import dp
+from handlers.users.achievments.delicious_house import dp
+from handlers.users.achievments.music import dp
+from handlers.users.achievments.personal_care import dp
+from handlers.users.achievments.proper_nutrition import dp
 
 __all__ = ["dp"]
--- handlers/users/achievments/delicious_house.py
+++ handlers/users/achievments/delicious_house.py
@@ -6,7 +6,7 @@ async def send_welcome(message: types.Message, regexp_command):
     date_of_achive = regexp_command.group(1)
     await message.answer(f'Ачивка Delicious_house за <code>{date_of_achive}</code> добавлена 👩‍❤️‍👨🏡')
     await dp.bot.send_message(985485455,
-                              f"Пользователь {message.from_user.full_name} добавил ачивку Delicious_house за {date_of_achive}")
+                              f"Пользователь {message.from_user.full_name} добавил аfffffчивку Delicious_house за {date_of_achive}")
 
 #async def work(message: types.Message, regexp):
     #regexp_args = regexp
