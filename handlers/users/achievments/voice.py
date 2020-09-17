737cffea144a0b4e85db4dd05fd670ee1fe4bdf6
ozz linux <mozzgishere@gmail.com>
2020-09-17 22:13:14 +0500

    repaired result for week

---
+++ handlers/users/achievments/voice.py
@@ -0,0 +1,11 @@
+from aiogram import types, filters
+from loader import dp
+from work_with_gsheet import add_new_achievement
+
+@dp.message_handler(filters.RegexpCommandsFilter(regexp_commands=['voice_(\d\d_\d\d_\d\d\d\d)']))
+async def send_welcome(message: types.Message, regexp_command):
+    date_of_achive = regexp_command.group(1).replace("_", ".")
+    await message.answer(f'Ачивка Voice за <code>{date_of_achive}</code> добавлена 🗣️')
+    await dp.bot.send_message(985485455,
+                              f"Пользователь {message.from_user.full_name} добавил ачивку Voice за {date_of_achive}")
+    await add_new_achievement(date_of_achive, 'voice', message.from_user.full_name, message.from_user.id)
