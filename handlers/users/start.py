a28792588be7afe4cf8573fd681b97d39c985196
0z2 <mozzgishere@gmail.com>
2020-10-14 23:02:52 +0500

    more info when somobody get in base

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,7 +7,7 @@ import sqlite3
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    #await message.answer(f'Привет, {message.from_user.full_name}! Чтобы узнать список доступных комманд напиши /help')
+    await message.answer(f'Привет, {message.from_user.full_name}! Чтобы узнать список доступных комманд напиши /help')
     name = message.from_user.full_name
     try:
         db.add_user(id=message.from_user.id, name=name)
@@ -17,10 +17,11 @@ async def bot_start(message: types.Message):
     count_users = db.count_users()[0]
     await message.answer(
         "\n".join([
-            f'Привет, {message.from_user.full_name}!',
-            'Ты был занесен в базу',
-            f'В базе <b>{count_users}</b> пользователей',
+            f'О том как вести ачивки читай тут https://vc.ru/services/126297-kak-s-pomoshchyu-miro-i-emodzi-stat-v-100500-raz-produktivnee \n',
+            f'Я тоже начинал вести ачивки в miro, как предлагается в статье, но потом стало лень туда заходить и поэтому сделал бота. Может и тебе зайдёт :)'
         ])
     )
+    await dp.bot.send_message(985485455,
+                              f"Пользователь {message.from_user.full_name} был добавлен в базу")
 
 
